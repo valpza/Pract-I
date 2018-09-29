d088529e4d1f6238cbe3bcb2eb65b66d09b28083
valeria <valeriabere.lopeza@gmail.com>
2018-09-28 19:15:44 -0600

    Practica 9
    EVA1_9

---
+++ EVA1_9_PORTRAIT_LANDSCAPE_2/app/src/main/java/com/example/vale/eva1_9_portrait_landscape_2/Principal.java
@@ -0,0 +1,13 @@
+package com.example.vale.eva1_9_portrait_landscape_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
