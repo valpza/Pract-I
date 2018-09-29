8a2c678430940946fea050342cb2f03c16346264
valeria <valeriabere.lopeza@gmail.com>
2018-09-28 19:07:59 -0600

    Practica 4
    EVA1_4

---
+++ EVA1_4_EVENTOS_/app/src/main/java/com/example/iowner/eva1_4_eventos/ClickListener.java
@@ -0,0 +1,15 @@
+package com.example.iowner.eva1_4_eventos;
+
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by iOwner on 03/09/2018.
+ */
+
+public class ClickListener implements View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+        Log.wtf("Desde clase","Evento click");
+    }
+}
