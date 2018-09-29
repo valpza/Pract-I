b56ed8dea703b3d5399d4eaf238d58c6e35fef12
valeria <valeriabere.lopeza@gmail.com>
2018-09-28 19:14:38 -0600

    Practica 9.5
    EVA1_9.5

---
+++ EVA1_9_PORTRAIT_LANDSCAPE/app/src/androidTest/java/com/example/iowner/eva1_9_portrait_landscape/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.example.iowner.eva1_9_portrait_landscape;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.iowner.eva1_9_portrait_landscape", appContext.getPackageName());
+    }
+}
